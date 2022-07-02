9c74479140e61f832e3af82c2280c16918987fc8
YuraS <yurastepanenko@gmail.com>
2022-07-02 13:20:13 +0300

    final Lec3

--- Lec1/main.go
+++ Lec1/main.go
@@ -1,7 +1,23 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello world!")
+// }
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var (
+		word1 = "имя"
+		word2 = "твое"
+		word3 = "мне"
+		word4 = "знакомо"
+	)
+
+	fmt.Println(word4, word3, word2, word1)
+	fmt.Println(word3, word1, word4, word2)
+	fmt.Println(word2, word4, word1, word3)
 }
